9f975d6c7b97edd4c2939b9afd13600039865900
Krzysztof <krzysiek@woelke.pl>
2023-01-19 19:36:15 +0100

    Add missing crud service methods

--- src/main/java/pl/medos/sensorApi/service/SensorService.java
+++ src/main/java/pl/medos/sensorApi/service/SensorService.java
@@ -2,9 +2,11 @@ package pl.medos.sensorApi.service;
 
 import org.springframework.stereotype.Service;
 import pl.medos.sensorApi.model.Sensor;
+import pl.medos.sensorApi.repository.SensorRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @Service
@@ -12,24 +14,45 @@ public class SensorService {
 
     private static final Logger LOGGER = Logger.getLogger(SensorService.class.getName());
 
+    private SensorRepository sensorRepository;
+
+    public SensorService(SensorRepository sensorRepository) {
+        this.sensorRepository = sensorRepository;
+    }
+
     public List listAll() {
+        LOGGER.info("()");
         return new ArrayList();
 
     }
-    
-    public void create(Sensor sensor) {
+
+    public Sensor create(Sensor sensor) {
+        LOGGER.info("create(" + sensor + ")");
+        Sensor createdSensor = sensorRepository.save(sensor);
+        LOGGER.info("create(...)");
+        return createdSensor;
 
     }
 
-    public void read(Long id) {
+    public Sensor read(Long id) {
+        LOGGER.info("read()");
+        Sensor readedSensor = sensorRepository.findById(id).orElseThrow();
+        LOGGER.info("read(...)");
+        return readedSensor;
 
     }
 
-    public void update(Sensor sensor) {
-
+    public Sensor update(Sensor sensor) {
+        LOGGER.info("update()");
+        Sensor updatedSensor = sensorRepository.findById(sensor.getId()).orElseThrow();
+        LOGGER.info("update(...)");
+        return updatedSensor;
     }
 
-    public void delete(Long id) {
-
+    public String delete(Long id) {
+        LOGGER.info("delete()");
+        sensorRepository.deleteById(id);
+        LOGGER.info("delete(...)");
+        return "Record " + id + " deleted!";
     }
 }
